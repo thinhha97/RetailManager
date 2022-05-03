accdc99497262f229eb6e788a5824ae44257c69d
thinhha-dev <havanthinh97@gmail.com>
2022-05-03 07:58:51 +0700

    Create WPF Login Form.

---
+++ RMDesktopUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMDesktopUI.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+        private string _username;
+        private string _password;
+
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                NotifyOfPropertyChange(() => Username);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+                NotifyOfPropertyChange(() => CanLogIn); 
+            }
+        }
+
+        public bool CanLogIn
+        {
+            get {
+                bool output = false;
+                if (Username?.Length > 0 && Password?.Length > 0)
+                {
+                    output = true;
+                }
+                return output;
+            }
+        }
+
+        public void LogIn()
+        {
+        }
+
+    }
+}
